07049c1a5f02761748fba052883b9446cfb12b30
Grace Panjaitan <60679474+gracepanjaitan@users.noreply.github.com>
2025-05-21 23:10:11 +0700

    fixing

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = defineConfig({
   e2e: {
     video: true,
-    specPattern: "**/*.feature",
+    specPattern: "cypress/e2e/features/**/*.feature",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("file:preprocessor", cucumber())
